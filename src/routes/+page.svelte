1b682c7ed0a7ff1a0a27ba9fcf1e8d3d27824d7c
fishsticks89 <61331006+fishsticks89@users.noreply.github.com>
2022-10-24 19:57:43 -0700

    newhome

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,57 +1,143 @@
 <script lang="ts">
+	import { flyin } from '$lib/transitions/flyin';
+	import { authState, expectingSignIn } from '$lib/auth/authState';
+	import { GoogleAuthProvider, signInWithRedirect } from 'firebase/auth';
+	import { auth, sets } from '$lib/firebase';
 	import { browser } from '$app/environment';
+	import {
+		getDocs,
+		query,
+		QueryDocumentSnapshot,
+		where,
+		type DocumentData
+	} from 'firebase/firestore';
+	import { get } from 'svelte/store';
+	import { createCloudSet, newset } from '$lib/data/db';
+	import { fade } from 'svelte/transition';
 	import { prefetch } from '$app/navigation';
-	import { getRedirectResult, onAuthStateChanged } from 'firebase/auth';
-	import { auth } from '$lib/firebase';
 	import Nav from '$lib/nav/Nav.svelte';
+	import AuthManager from '$lib/auth/AuthManager.svelte';
+	import Runner from '$lib/util/Runner.svelte';
 
-	if (browser) prefetch('./learn');
-
-	onAuthStateChanged(auth, (newuser) => {
-		if (newuser) window.location.replace('./home');
-	});
-
-	getRedirectResult(auth)
-		.then((result) => {
-			if (result) window.location.replace('./home');
-		})
-		.catch((error) => {
-			// ...
-		});
+	let docs: QueryDocumentSnapshot<DocumentData>[] = [];
 </script>
 
-<Nav>
-	<a
-		href="./home"
-		class="topbutton"
-		on:click={() => {
-			window.location.replace('./home');
-		}}><p class="gs">Get Started</p></a
-	></Nav
->
-<h2>
-	<p class="punchline">study effectively</p>
-	<p class="punchline1">
-		in minutes <img src="./icons/rocket.png" alt="rocket" style:height={50 + 'px'} />
-	</p>
-	<a
-		href="./learn"
-		on:click={() => {
-			window.location.replace('./learn');
-		}}>Start Studying</a
-	>
-</h2>
+{#if $authState === null && !expectingSignIn && !auth.currentUser && browser}
+	<h2>
+		<p class="punchline">Flashcards, Quizzes</p>
+		<p class="punchline1">
+			and more <img src="./icons/rocket.png" alt="rocket" style:height={50 + 'px'} />
+		</p>
+		<button
+		class="startbutt"
+			on:click={() => {
+				signInWithRedirect(auth, new GoogleAuthProvider());
+			}}>Start Studying</button
+		>
+	</h2>
+{/if}
 
-<!-- <img
-    on:click={() => {
-        window.location.replace("https://github.com/fishsticks89/openlearn");
-    }}
-    style={"cursor: pointer;border-radius: 50%;filter: invert();position: fixed;right: 1rem;top: 1rem;width: 2rem;height: 2rem;"}
-    src="./ghlogo.png"
-    alt=""
-    srcset=""
-/> -->
+{#if $authState != null}
+	<Runner
+		enter={() => {
+			getDocs(
+				query(sets, where('user', '==', get(authState)?.uid), where('contents', '!=', ''))
+			).then((e) => {
+				docs = e.docs;
+			});
+		}}
+	/>
+	<div class="holder" id="create">
+		<button
+			on:click={() => {
+				const uid = $authState?.uid;
+				if (uid) {
+					const set = {
+						user: uid,
+						name: '',
+						contents: newset().contents
+					};
+					createCloudSet(set).then((setref) => {
+						window.location.replace('/set/' + setref.id);
+					});
+				}
+			}}
+			class="newset"
+			in:fade={{ duration: 300 }}
+			out:fade={{ duration: 100 }}>New Set</button
+		>
+		{#each docs as doc}
+			<div
+				in:flyin={{ isin: true, additionalTransforms: '' }}
+				out:flyin={{ isin: false, additionalTransforms: '' }}
+				class="setholder"
+				on:mouseover={() => {
+					prefetch('/set/' + doc.id);
+				}}
+				on:focus
+				on:click={() => {
+					window.location.replace('/set/' + doc.id);
+				}}
+			>
+				<p style:margin-top="0px">{doc.get('name') != '' ? doc.get('name') : 'Untitled'}</p>
+				<button class="open">Open</button>
+			</div>
+		{/each}
+	</div>
+{/if}
+
+<Nav>
+	<AuthManager />
+</Nav>
+
 <style>
+	.newset {
+		margin-block: 1rem;
+		padding: 0.7rem;
+		padding-inline: 1.2rem;
+		background-color: var(--glass);
+
+		border: 0px solid transparent;
+		border-radius: var(--round);
+
+		font-family: 'GilroyBold', sans-serif;
+	}
+	.open {
+		display: inline;
+		position: absolute;
+		right: 0.4rem;
+		padding: 0.7rem;
+		padding-inline: 1.2rem;
+		top: 50%;
+		translate: 0px -50%;
+		background-color: var(--glass);
+
+		border: 0px solid transparent;
+		border-radius: var(--round);
+
+		font-family: 'GilroyBold', sans-serif;
+	}
+	.setholder {
+		position: relative;
+		text-align: left;
+		display: block;
+		width: calc(100% - 1.4rem);
+		padding: 0.7rem;
+
+		border-radius: var(--round);
+		background-color: var(--emp);
+	}
+	.holder {
+		width: calc(70vw + 8vh);
+		max-width: 50rem;
+		height: fit-content;
+		margin: 0px;
+		margin-left: 50vw;
+		transform: translateX(-50%);
+		padding-top: 4rem;
+
+		overflow: visible;
+	}
 	h2 {
 		position: fixed;
 		font-family: 'GilroyBold', sans-serif;
@@ -71,18 +157,7 @@
 	.punchline1 {
 		margin: 0rem;
 	}
-	.gs {
-		margin: 0px;
-	}
-	.topbutton {
-		background-color: var(--emp);
-		padding: 0.6rem;
-		padding-inline: 1.4rem;
-		background-color: var(--light);
-		color: var(--emp);
-		font-size: 0.9rem;
-	}
-	a {
+	.startbutt {
 		background-color: var(--accent);
 		border-radius: var(--round);
 		border: 0px;
